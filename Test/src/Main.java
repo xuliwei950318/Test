43223522c957976d6ac112f30a1ae275ddd46df7
xuliwei950318 <1051597421@qq.com>
2017-02-26 15:19:17 +0800

    第er次

---
+++ Test/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.function.BinaryOperator;
+
+/**
+ * Created by xuliwei on 2017/2/26.
+ */
+public class Main {
+    BinaryOperator<Integer> sum = (x,y)->x+y;
+
+    public static void main(String[] args) {
+        
+    }
+}
